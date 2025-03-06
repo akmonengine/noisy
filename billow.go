7b14ea4af933018b6b22f591f016ac75e593e510
ramine <ramine.agoune@teksial.com>
2025-03-06 22:04:24 +0100

    feat: billow noise

---
+++ billow.go
@@ -0,0 +1,27 @@
+package noisy
+
+import "math"
+
+type Billow struct {
+	Frequency, Lacunarity, Persistence float64
+	OctaveCount, Seed                  int
+}
+
+func (billow Billow) GetValue(x, y, z float64) float64 {
+	value := 0.0
+	persistence := 1.0
+	frequency := billow.Frequency
+	seed := billow.Seed
+
+	for range billow.OctaveCount {
+		noise := persistence * getNoise(seed, x*frequency, y*frequency, z*frequency)
+		value += math.Abs(noise)
+		// prepapre the persistency & frequency for the next octave
+		persistence *= billow.Persistence
+		frequency *= billow.Lacunarity
+		// offset the seed to avoid seeing patterns
+		seed++
+	}
+
+	return value
+}
