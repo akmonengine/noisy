6909f07ef80cbbc6dcaf0489be7e9a05253c94d2
Ramine Agoune <ramine.agoune@solidlynx.com>
2025-03-07 20:38:27 +0100

    chore: add documentation in README

--- source_test.go
+++ source_test.go
@@ -3,29 +3,52 @@ package noisy
 import (
 	"fmt"
 	"image/color"
+	"math/rand/v2"
 	"testing"
 )
 
 func TestGenerate(t *testing.T) {
-	generator := RidgedMulti{
-		Frequency:   5.0,
-		Lacunarity:  2.0,
-		Persistence: 0.5,
-		Offset:      0.3,
-		Gain:        0.9,
-		OctaveCount: 6,
-		Seed:        48498,
+	seed := rand.Int()
+	generator := Multiply{
+		Sphere{
+			Frequency: 2.0,
+			OffsetX:   -1.0,
+			OffsetY:   -1.0,
+			OffsetZ:   -1.0,
+		},
+		Clamp{
+			SourceA: Add{
+				Perlin{
+					Frequency:   1.1,
+					Lacunarity:  2.0,
+					Persistence: 0.5,
+					OctaveCount: 6,
+					Seed:        seed,
+				},
+				RidgedMulti{
+					Frequency:   3.0,
+					Lacunarity:  2.0,
+					Persistence: 0.5,
+					Offset:      0.5,
+					Gain:        0.8,
+					OctaveCount: 5,
+					Seed:        seed,
+				},
+			},
+			Min: 0.0,
+			Max: 1.0,
+		},
 	}
 
 	err := RenderImg(generator, map[float64]color.RGBA{
 		-1.0: {0, 0, 0, 255},
-		0.0:  {100, 120, 0, 255},
-		0.5:  {120, 200, 0, 255},
+		0.0:  {0, 0, 0, 255},
+		0.5:  {255, 255, 255, 255},
 	}, "test.png", 1024, 1024)
 
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
-	fmt.Println(generator.GetValue(0.5, 0.5, 0.0))
+	fmt.Println(seed, generator.GetValue(0.5, 0.5, 0.0))
 }
