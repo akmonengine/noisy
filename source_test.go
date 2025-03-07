004fb3633ee4e165cf0d73e2f627b3b63d108350
Ramine Agoune <ramine.agoune@solidlynx.com>
2025-03-07 17:24:46 +0100

    fix: clean code for linter

--- source_test.go
+++ source_test.go
@@ -17,11 +17,15 @@ func TestGenerate(t *testing.T) {
 		Seed:        48498,
 	}
 
-	RenderImg(generator, map[float64]color.RGBA{
+	err := RenderImg(generator, map[float64]color.RGBA{
 		-1.0: {0, 0, 0, 255},
 		0.0:  {100, 120, 0, 255},
 		0.5:  {120, 200, 0, 255},
 	}, "test.png", 1024, 1024)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
 	fmt.Println(generator.GetValue(0.5, 0.5, 0.0))
 }
